c5307266000760e67ea8a4dc500a69a3b40fbbb6
anderson155081 <anderson155081@gmail.com>
2020-06-01 00:29:58 +0800

    Update

--- src/event.py
+++ src/event.py
@@ -1,8 +1,44 @@
-def event():
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
-            if playery + playerHeight >= BASEY - 1:
-                return
+import cv2
+import dlib
+from imutils import face_utils
+from math import hypot
+
+cap = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+EYE_BLINKED = 4
+
+
+
+def get_blinking_ratio(frame, eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+    hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+    ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+
+    hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+    ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+
+    ratio = hor_line_lenght / ver_line_lenght
+    return ratio
+
+def midpoint(p1 ,p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+def eyeevent():
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
+    blinked = False
+    for face in faces:
+        landmarks = predictor(gray, face)
+        shape = face_utils.shape_to_np(landmarks)
+        left_eye_ratio = get_blinking_ratio(frame, [36, 37, 38, 39, 40, 41], landmarks)
+        right_eye_ratio = get_blinking_ratio(frame, [42, 43, 44, 45, 46, 47], landmarks)
+        blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
+        if blinking_ratio > EYE_BLINKED:
+            blinked = True
